b6ac65e5fccdc0d45aed38c3c0d89faab9c7b527
Matheus Leite de Menezes <matheus.menezes@cabal.com.br>
2020-09-08 16:10:15 -0300

    first steps using composer

--- app/src/main/java/com/matheusleite/composeapp/MainActivity.kt
+++ app/src/main/java/com/matheusleite/composeapp/MainActivity.kt
@@ -3,36 +3,59 @@ package com.matheusleite.composeapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.matheusleite.composeapp.ui.ComposeAppTheme
+import java.lang.reflect.Modifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "Composer")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeAppTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", color = Color.Blue, modifier = androidx.compose.ui.Modifier.padding(26.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeAppTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
